0e19395e27838eeaa32177658bfc9355421f85fb
Shivangi Ranjan <shivangiec1098@gmail.com>
2021-12-07 20:19:33 +0530

    Solved using Switch Case

--- EmployWage.java
+++ EmployWage.java
@@ -1,18 +1,23 @@
-public class EmployWage{
+public class EmployWage {
+    public static final int part_time=1;
+    public static final int full_time=2;
+    public static final int emp_rate=20;
+
     public static void main(String[] args) {
-        int part_time = 1;
-        int full_time = 2;
-        int emp_rate = 20;
         int emp_hrs, emp_wage;
-        double check = Math.floor(Math.random() * 10) % 3;
-        System.out.println("Employ Attendance Status=" + check);
-        if (check == part_time)
-            emp_hrs = 4;
-        else if (check == full_time)
-            emp_hrs = 8;
-        else
-            emp_hrs = 0;
-        emp_wage = emp_hrs * emp_rate;
+        int  check= (int) Math.floor(Math.random() *10)%3;
+        System.out.println("Employ Attendance Status="+check);
+        switch (check) {
+            case part_time:
+                emp_hrs = 4;
+                break;
+            case full_time:
+                emp_hrs = 8;
+                break;
+            default:
+                emp_hrs = 0;
+        }
+        emp_wage=emp_hrs*emp_rate;
         System.out.println("Employ Wage: " + emp_wage);
     }
 }
