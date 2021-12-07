d386cd1ac66b59554f0bcb82bf9d0e585b40a031
Shivangi Ranjan <shivangiec1098@gmail.com>
2021-12-07 20:34:04 +0530

    Calculate wages for a month assuming 20 days in a months

--- EmployWage.java
+++ EmployWage.java
@@ -1,23 +1,31 @@
-public class EmployWage {
+public class EmployWage{
     public static final int part_time=1;
     public static final int full_time=2;
     public static final int emp_rate=20;
+    public static final int work_day=2;
 
     public static void main(String[] args) {
         int emp_hrs, emp_wage;
-        int  check= (int) Math.floor(Math.random() *10)%3;
-        System.out.println("Employ Attendance Status="+check);
-        switch (check) {
-            case part_time:
-                emp_hrs = 4;
-                break;
-            case full_time:
-                emp_hrs = 8;
-                break;
-            default:
-                emp_hrs = 0;
+        int tot_wage=0;
+        for(int i=0;i<work_day;i++)
+        {
+            int  check= (int) Math.floor(Math.random() *10)%3;
+            System.out.println("Employ Attendance Status="+check);
+            switch (check) {
+                case part_time:
+                    emp_hrs = 4;
+                    break;
+                case full_time:
+                    emp_hrs = 8;
+                    break;
+                default:
+                    emp_hrs = 0;
+            }
+            emp_wage=emp_hrs*emp_rate;
+            tot_wage +=emp_wage;
+            System.out.println("Employ Wage: " + emp_wage);
         }
-        emp_wage=emp_hrs*emp_rate;
-        System.out.println("Employ Wage: " + emp_wage);
+        System.out.println("Total Emp Wage: " + tot_wage);
+
     }
 }
