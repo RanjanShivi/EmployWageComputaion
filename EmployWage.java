8d72552f4a68126ad18a3e3b708865b4a5876c55
Shivangi Ranjan <shivangiec1098@gmail.com>
2021-12-07 20:09:36 +0530

    Added part time employ and wage

--- EmployWage.java
+++ EmployWage.java
@@ -1,16 +1,18 @@
-public class EmployWage {
+public class EmployWage{
     public static void main(String[] args) {
-        int full_time=1;
-        int emp_rate=20;
+        int part_time = 1;
+        int full_time = 2;
+        int emp_rate = 20;
         int emp_hrs, emp_wage;
-        double check=Math.floor(Math.random() *10)%2;
-        System.out.println("Employ Attendance Status="+check);
-        if (check==full_time)
-            emp_hrs=8;
+        double check = Math.floor(Math.random() * 10) % 3;
+        System.out.println("Employ Attendance Status=" + check);
+        if (check == part_time)
+            emp_hrs = 4;
+        else if (check == full_time)
+            emp_hrs = 8;
         else
-            emp_hrs=0;
-        emp_wage=emp_hrs*emp_rate;
+            emp_hrs = 0;
+        emp_wage = emp_hrs * emp_rate;
         System.out.println("Employ Wage: " + emp_wage);
     }
 }
-
